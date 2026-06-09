b26cbb508a65723f6d3a163318cab5fae4e58e5f
landercor <asprillafabian034@gmail.com>
2026-06-09 00:22:55 -0500

    Done: ultimos cambios en relacion a la Proyecto ECCI implementacion de carousel, icons y el glosario tecnico.

--- public/js/Scroll_UD.js
+++ public/js/Scroll_UD.js
@@ -1,5 +1,6 @@
 window.addEventListener('scroll', () => {
     const navbar = document.getElementById('navbar-z');
+    if (!navbar) return;
     if (window.scrollY > 50) {
         navbar.classList.add('scrolled');
     } else {
